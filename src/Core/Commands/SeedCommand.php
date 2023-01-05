0e48d6b752bb6eb14c3dc1b8c9ed5a5c009d2fe3
danielhe4rt <hey@danielheart.dev>
2023-01-05 16:05:12 -0300

    feat: command seed and sensor domain

--- src/Core/Commands/SeedCommand.php
+++ src/Core/Commands/SeedCommand.php
@@ -8,32 +8,41 @@ use App\Owner\OwnerFactory;
 use App\Owner\OwnerRepository;
 use App\Pet\PetFactory;
 use App\Pet\PetRepository;
+use App\Sensor\SensorFactory;
+use App\Sensor\SensorRepository;
 
 class SeedCommand extends AbstractCommand
 {
 
-    const AMOUNT_BASE = 50;
+    const AMOUNT_BASE = 10000;
+
     public function handle(array $args): int
     {
         $ownerRepository = new OwnerRepository();
         $petRepository = new PetRepository();
-
+        $sensorRepository = new SensorRepository();
 
         for ($i = 0; $i <= self::AMOUNT_BASE; $i++) {
             $ownerDTO = OwnerFactory::make();
-            $petDTO =  PetFactory::make(['owner_id' => $ownerDTO->id]);
+            $petDTO = PetFactory::make(['owner_id' => $ownerDTO->id]);
+            $sensorDTOs = SensorFactory::makeMany(5, [
+                'pet_id' => $petDTO->id,
+                'owner_id' => $petDTO->id
+            ]);
 
             $ownerRepository->create($ownerDTO);
             $this->info(sprintf('Owner %s', $ownerDTO->id));
 
             $petRepository->create($petDTO);
             $this->info(sprintf('Pet: %s | Owner %s', $petDTO->id, $petDTO->ownerId));
+
+            foreach ($sensorDTOs as $sensorDTO) {
+                $sensorRepository->create($sensorDTO);
+                $this->info(sprintf('Sensor: %s | Pet %s', $sensorDTO->id, $sensorDTO->petId));
+            }
         }
 
         while (true) {
-
-
-
             sleep(1);
         }
     }
